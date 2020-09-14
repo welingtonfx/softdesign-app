0b11a25f76f09807fafa24d7a5fe5dca17c28199
Welington Wilton <welingtonfx@gmail.com>
2020-09-13 22:39:11 -0300

    Testes

--- SoftDesignApp/Test/Aplicacao.Test/ServicoAplicacaoApplicationTest.cs
+++ SoftDesignApp/Test/Aplicacao.Test/ServicoAplicacaoApplicationTest.cs
@@ -1,11 +1,10 @@
 ﻿using Aplicacao;
 using AutoFixture;
 using Dominio.Interface;
-using Dominio.Interface.Infra;
 using Dominio.Interface.Infra.Repositorio;
 using Dominio.Model;
 using Dominio.ViewModel;
-using MongoDB.Bson;
+using Infra.Repositorio;
 using Moq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -17,17 +16,12 @@ namespace Test.Aplicacao.Test
     {
         private readonly Fixture _fixture;
         private readonly Mock<IRepositorioApplication> _repositorioApplicationMock;
-
-        private readonly Mock<IDatabaseSettings> _databaseSettingsMock;
-
         private readonly Mock<IApplicationValidator> _applicationValidator;
         private readonly ServicoAplicacaoApplication _servicoAplicacaoApplication;
 
         public ServicoAplicacaoApplicationTest()
         {
             _fixture = new Fixture();
-            _databaseSettingsMock = new Mock<IDatabaseSettings>();
-
             _repositorioApplicationMock = new Mock<IRepositorioApplication>();
 
             _applicationValidator = new Mock<IApplicationValidator>();
@@ -47,8 +41,6 @@ namespace Test.Aplicacao.Test
             Assert.NotEmpty(retorno);
         }
 
-        // get vazio
-
         [Fact]
         public async Task GetById_ShouldReturnOneApplication()
         {
@@ -65,21 +57,39 @@ namespace Test.Aplicacao.Test
             Assert.NotEmpty(retorno.Id);
         }
 
-        [Fact]
-        public async Task Insert_ShouldReturnSavedObject()
-        {
-            var applicationViewModel = _fixture.Create<ApplicationViewModel>();
-            var application = (ApplicationModel)applicationViewModel;
+        //[Fact]
+        //public async Task Insert_ShouldReturnSavedObject()
+        //{
+        //    var applicationViewModel = _fixture.Create<ApplicationViewModel>();
+        //    var application = (ApplicationModel)applicationViewModel;
 
-            _repositorioApplicationMock.Setup(p => p.Insert(application)).ReturnsAsync(application);
+        //    _repositorioApplicationMock.Setup(p => p.Insert(application)).ReturnsAsync(application);
 
-            var retorno = await _servicoAplicacaoApplication.Insert(applicationViewModel);
+        //    var retorno = await _servicoAplicacaoApplication.Insert(applicationViewModel);
 
-            Assert.IsType<ApplicationViewModel>(retorno);
-            Assert.NotNull(retorno);
-            Assert.NotNull(retorno.Id);
-            Assert.NotEmpty(retorno.Id);
-            Assert.Equal(24, retorno.Id.Length);
-        }
+        //    Assert.IsType<ApplicationViewModel>(retorno);
+        //    Assert.NotNull(retorno);
+        //    Assert.NotNull(retorno.Id);
+        //    Assert.NotEmpty(retorno.Id);
+        //    Assert.Equal(24, retorno.Id.Length);
+        //}
+
+        //[Fact]
+        //public async Task Update_ShouldReturnSavedObject()
+        //{
+        //    var applicationViewModel = _fixture.Create<ApplicationViewModel>();
+        //    var application = (ApplicationModel)applicationViewModel;
+        //    var application2 = _fixture.Create<ApplicationModel>();
+
+        //    _repositorioApplicationMock.Setup(p => p.Update(application.Id, application)).ReturnsAsync(application2);
+
+        //    var retorno = await _servicoAplicacaoApplication.Update(application.Id, applicationViewModel);
+
+        //    Assert.IsType<ApplicationViewModel>(retorno);
+        //    Assert.NotNull(retorno);
+        //    Assert.NotNull(retorno.Id);
+        //    Assert.NotEmpty(retorno.Id);
+        //    Assert.Equal(24, retorno.Id.Length);
+        //}
     }
 }
